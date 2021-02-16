3aa1122533ca6991d5869d7a21779d574592f14f
Christopher Yancey <cyancey76@gmail.com>
2021-02-16 09:35:30 -0800

    update config with correct room url

--- config.js
+++ config.js
@@ -8,8 +8,8 @@
 var ftconfigs = {
    roomName: "Fuzzy Robot Disco Bear", // what you'd like your room name to be displayed as
    roomNameShort: "FRDB", // if you have a long room name, put a shorter version here (like 2 words probably max).
-   defaultAlbumArtUrl: "http://cyancey76.github.io/img/discoball.png", // url of an image to use for tracks with no album art. should be a square and at least 500x500 px for best results.
-   roomInfoUrl: "https://www.fuzzyrobotdiscobear.com/music/", // Change what the "?" button at the top of the page links to. This can be any URL
+   defaultAlbumArtUrl: "/img/discoball.png", // url of an image to use for tracks with no album art. should be a square and at least 500x500 px for best results.
+   roomInfoUrl: "https://music.fuzzyrobotdiscobear.com/", // Change what the "?" button at the top of the page links to. This can be any URL
    firebase:{ // put the info from your Firebase app here. can be found in the Firebase console
     apiKey: "AIzaSyDQWEtmBPi7w_R0-foS1WnDvBqKg9rgXcQ",
     authDomain: "fuzzy-robot-disco-bear.firebaseapp.com",
